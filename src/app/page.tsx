1f67b34fea770108b6b3e410a63a90da7823fdeb
c i y e k <106911287+muhammadarii@users.noreply.github.com>
2025-03-13 16:09:29 +0700

    feat: Add ListCard component and integrate with Home page layout (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import ListCard from "@/components/ListCard";
 import SearchBox from "@/components/SearchBox";
 
 export default function Home() {
   return (
-    <div>
-      <SearchBox className="py-[25px] px-[130px]" />
+    <div className="flex flex-col">
+      <div className="py-[25px] px-[130px]">
+        <SearchBox />
+        <ListCard />
+      </div>
     </div>
   );
 }
---
+++ src/types/types.ts
@@ -0,0 +1,3 @@
+export type SearchBoxProps = {
+  className?: string;
+};
