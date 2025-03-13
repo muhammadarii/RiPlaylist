886b8a2772d823631ef478383a72773a5a06ed68
ciyek <aripurnomo606@gmail.com>
2025-03-13 15:39:11 +0700

    first commit

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+
+type Props = {
+  className?: string;
+};
+
+const SearchBox: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={`${className} flex items-center gap-4 rounded-full px-4 py-2 focus-within:border-blue-500`}
+    >
+      <BiSearch className="text-gray-500 text-lg w-[20px] h-[20px]" />
+      <input
+        type="text"
+        placeholder="Search"
+        className="w-full min-w-0 border-none outline-none bg-transparent text-white placeholder-gray-500 text-[14px]"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
