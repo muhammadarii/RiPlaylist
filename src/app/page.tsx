04eefe5357dc20d1331701f3ba72b0f2c103d947
ciyek <aripurnomo606@gmail.com>
2025-03-19 21:20:26 +0700

    feat: Add Navbar component and update layout; modify font and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const page = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 };
 
 export default page;
--- src/components/parts/Navbar.tsx
+++ src/components/parts/Navbar.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import Link from "next/link";
+
+const navLinks = [
+  { href: "/", label: "Home" },
+  { href: "/", label: "Properties" },
+  { href: "/", label: "About Us" },
+  { href: "/", label: "Login" },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full fixed flex justify-between items-center p-4">
+      <Link href="/" className="font-bold text-2xl">
+        Dudeshape
+      </Link>
+      <div className="flex gap-10 font-medium items-center">
+        {navLinks.map(({ href, label }) => (
+          <Link key={label} href={href}>
+            {label}
+          </Link>
+        ))}
+        <Link
+          href="/"
+          className="bg-white w-30 h-12 flex justify-center items-center rounded-xl text-black"
+        >
+          Get Started
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
