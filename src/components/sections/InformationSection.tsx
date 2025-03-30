6e67db081b518d099441ce1e7d3a1565dac6fc48
ciyek <aripurnomo606@gmail.com>
2025-03-30 14:36:50 +0700

    feat: Update spacing in InformationSection and PopularSection for improved layout

--- src/components/sections/InformationSection.tsx
+++ src/components/sections/InformationSection.tsx
@@ -10,7 +10,7 @@ const InformationSection = () => {
         </div>
         <div className="flex flex-col items-start justify-center">
           <h1 className="text-[48px] font-semibold">Get To Know Us More</h1>
-          <p className="text-[18px] font-[#BDBDBD] w-[618px]">
+          <p className="text-[18px] font-[#BDBDBD] w-[618px] mt-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
