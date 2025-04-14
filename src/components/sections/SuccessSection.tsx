6acacb36525430336f7ba903652110f50d27d1c7
ciyek <aripurnomo606@gmail.com>
2025-04-14 20:12:38 +0700

    feat: Update PopularSection and SuccessSection layouts for improved responsiveness and visual consistency

--- src/components/sections/SuccessSection.tsx
+++ src/components/sections/SuccessSection.tsx
@@ -1,7 +1,7 @@
 const SuccessSection = () => {
   return (
-    <div className="flex flex-col items-start mt-[100px] mx-[175px]">
-      <h1 className="w-[401px] text-[38px] font-semibold">
+    <div className="flex flex-col items-start mt-[50px] lg:mt-[100px] lg:mx-[175px]">
+      <h1 className="lg:w-[401px] text-[24px] lg:text-[38px] font-semibold px-4 lg:px-0">
         We Build More Project Successfully
       </h1>
     </div>
